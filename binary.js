9e79affa39c33c913110f4a26c0a89e5be93a2d8
Kevin Himawan Hie <kevinhimawanhie@Kevins-MacBook-Pro.local>
2018-02-06 14:51:05 +0700

    selection sort

--- binary.js
+++ binary.js
@@ -3,28 +3,33 @@
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
+
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  
+  let arrayNumb = arr
+
+  for(let i = 0;i < arrayNumb.length; i++){
+    let nilaiMinimum = arrayNumb[i]
+    let nilaiMinimumIndex = i
+    let count = 0
+    for(let j = i+1;j < arrayNumb.length;j++){
+      
+       if(arrayNumb[j] < nilaiMinimum){
+        nilaiMinimum = arrayNumb[j]
+        nilaiMinimumIndex = j
+        count++
+       } 
+    }
+
+    if(count > 0){
+      let tmp = arrayNumb[i]
+      arrayNumb[i] = arrayNumb[nilaiMinimumIndex]
+      arrayNumb[nilaiMinimumIndex] = tmp
+    }
+  }
+
+  return arrayNumb
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
-}
-
-var arrayGenapSorted = ownSort(testArrayGenap)
-var arrayGanjilSorted = ownSort(testArrayGanjil)
-
-// Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
-
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
-
-module.exports = {
-  binary_search
-}
+console.log(ownSort(testArrayGenap))
+console.log(ownSort(testArrayGanjil))
