abf54394c03c36320aaa8335e5352228cd3172b4
Ervan Adetya <vaanz.adetya@gmail.com>
2018-02-06 12:08:30 +0700

    First Commit

--- binary.js
+++ binary.js
@@ -1,30 +1,26 @@
 'use strict'
 
-var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
-var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
+function selectionSort(array) {
+  var arr=[];
 
-function ownSort(arr) {
-  // Your sorting code
-  return arr
+  while(array.length!==0) {
+    var min=findMin(array);
+    arr.push(min);
+    array.splice(array.indexOf(min),1);
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function findMin(arr) {
+  var min=Number.MAX_SAFE_INTEGER;
+  for(var i in arr) {
+    if(min>arr[i]) min=arr[i];
+  }
+  return min;
 }
 
-var arrayGenapSorted = ownSort(testArrayGenap)
-var arrayGanjilSorted = ownSort(testArrayGanjil)
+var arr1=[33,2,52,106,73]
+var arr2=[13,5,22,99,11]
 
-// Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
-
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
-
-module.exports = {
-  binary_search
-}
+console.log(selectionSort(arr1))
+console.log(selectionSort(arr2))
