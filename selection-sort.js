213df59fd41b24b093976f6dda633e8522ff5f73
agungcaproex <agung.caproex@gmail.com>
2018-02-06 16:11:54 +0700

    upload selection-sort

---
+++ selection-sort.js
@@ -0,0 +1,21 @@
+function selectSort(input){
+  var temp = []
+  var minIndex = ""
+
+  for(var i=0; i<input.length; i++){
+    minIndex = i
+    debugger
+    for(var j=i+1; j<input.length; j++){
+      if(input[j] < input[minIndex]){
+        minIndex = j
+        debugger
+      }
+    }
+    temp = input[i]
+    input[i] = input[minIndex]
+    input[minIndex] = temp
+  }
+  return input
+}
+console.log(selectSort([33,22,52,106,73]));
+console.log(selectSort([13,5,22,99,11]));
