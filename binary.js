f045f252b16646e56016a919533ac0e088baa283
root <irsyadpahlapi28@gmail.com>
2018-02-06 03:22:16 -0500

    ulpload file binary js

--- binary.js
+++ binary.js
@@ -4,26 +4,69 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
+  for(let i=0;i<arr.length-1;i++){
+    var index=i
+    for(let j=i+1;j<arr.length;j++){
+      if(arr[j]<arr[index]){
+        index=j
+      }
+    }
+    if(index != i){
+      var insert = arr[i]
+      arr[i]=arr[index]
+      arr[index] = insert
+    }
+
+  }
   return arr
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  var awal =array[Math.floor(array.length/2)-1]
+  if(search>awal){
+    var kedua =array[((Math.floor(array.length/2)/2))]
+    if(search<kedua){}
+    else if(search>kedua){}
+    else if(search===kedua){
+      return (((Math.floor(array.length/2)/2)))
+    }
+  }
+  else if(search<awal){
+    var kedua =array[((Math.floor(array.length/2)/2))]
+    if(search<kedua){
+      var ketiga =array[(((Math.floor(array.length/2)/2)/2))]
+      if(search<ketiga){
+        var keempat =array[Math.floor(((((array.length/2)/2)/2)/2))]
+        if(search<keempat){}
+        else if(search>keempat){}
+        else if(search===keempat){
+          return Math.floor(((((array.length/2)/2)/2)/2))
+        }
+      }
+    }
+    else if(search>kedua){}
+    else if(search===kedua){
+      return (((Math.floor(array.length/2)/2)))
+    }
+
+  }
+  else if(awal===search){
+    return Math.floor(array.length/2)-1
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
-
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binary_search(8, arrayGenapSorted))//0
+console.log(binary_search(10, arrayGenapSorted))//2
+console.log(binary_search(33, arrayGenapSorted))//-1
+//
+console.log(binary_search(53, arrayGanjilSorted))//3
+console.log(binary_search(3, arrayGanjilSorted))//0
+console.log(binary_search(2, arrayGanjilSorted))//-1
 
 module.exports = {
   binary_search
