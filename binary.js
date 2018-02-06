2ffa33b20c1a361716a5c3c64f24af59c045d431
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-06 16:37:20 +0700

    selection sort

--- binary.js
+++ binary.js
@@ -1,30 +1,29 @@
-'use strict'
+function selectionSort(input) {
+  var arr=[];
+  var temp=0;
+ 
 
-var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
-var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
-
-function ownSort(arr) {
-  // Your sorting code
-  return arr
+for(var j=0;j<input.length;j++)
+  {
+    temp=j;
+    for(var i=j+1;i<input.length;i++)
+        {
+          if(input[i]<input[temp])
+              {
+               // min=input[i];
+                temp=i;
+              }
+       
+          }
+     
+    arr=input[j];
+    input[j]=input[temp];
+    input[temp]=arr;
+      
+  }
+      return input;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
-}
 
-var arrayGenapSorted = ownSort(testArrayGenap)
-var arrayGanjilSorted = ownSort(testArrayGanjil)
-
-// Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
-
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
-
-module.exports = {
-  binary_search
-}
+console.log(selectionSort([33,2,52,106,73]));
+console.log(selectionSort([13,5,22,99,11]));
