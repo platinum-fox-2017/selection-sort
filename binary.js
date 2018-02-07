8c0646da291c35e345932d5209dac41722e2ceea
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-07 11:51:25 +0700

    REVISI SELECTION

--- binary.js
+++ binary.js
@@ -2,21 +2,22 @@ function selectionSort(input) {
   var arr=[];
   var temp=0;
 
-  for(var j=0;j<input.length;j++)
-    {
+  for(var j=0;j<input.length;j++){
         temp=j;
+
         for(var i=j+1;i<input.length;i++)
           {
-              if(input[i]<input[temp])
-                {
+              if(input[i]<input[temp]){
                   temp=i;
-                }
+              }
           }
+
         arr=input[j];
         input[j]=input[temp];
         input[temp]=arr;
-    }
-    return input;
+  }
+
+  return input;
 }
 
 console.log(selectionSort([33,2,52,106,73]));
