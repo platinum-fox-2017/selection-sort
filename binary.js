e2a068c96c2ef1243cd6457f1d760ea6ab9e75d3
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-07 11:06:20 +0700

    REVISI BINARY SEARCH

--- binary.js
+++ binary.js
@@ -1,27 +1,25 @@
 function selectionSort(input) {
   var arr=[];
   var temp=0;
- 
 
-for(var j=0;j<input.length;j++)
-  {
-    temp=j;
-    for(var i=j+1;i<input.length;i++)
-        {
-          if(input[i]<input[temp])
-              {
-               // min=input[i];
-                temp=i;
-              }
-       
+
+  for(var j=0;j<input.length;j++)
+    {
+        temp=j;
+        for(var i=j+1;i<input.length;i++)
+          {
+              if(input[i]<input[temp])
+                {
+                  temp=i;
+                }
+
           }
-     
-    arr=input[j];
-    input[j]=input[temp];
-    input[temp]=arr;
-      
-  }
-      return input;
+        arr=input[j];
+        input[j]=input[temp];
+        input[temp]=arr;
+    }
+
+    return input;
 }
 
 
